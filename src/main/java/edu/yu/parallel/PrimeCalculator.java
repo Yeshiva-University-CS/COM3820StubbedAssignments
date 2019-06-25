4d071285318e04145585c323baf941941c62f6ef
avraham <avrahamleff@gmail.com>
2019-06-25 16:38:43 -0400

    Interface must be declared "public" (was package protected): non-package clients should be able to access

--- src/main/java/edu/yu/parallel/PrimeCalculator.java
+++ src/main/java/edu/yu/parallel/PrimeCalculator.java
@@ -3,14 +3,13 @@ package edu.yu.parallel;
 /** Interface defining an API for calculating primes
  *
  */
-interface PrimeCalculator {
+public interface PrimeCalculator {
 
-  /** Returns the number of primes in the specified range.
-   *
-   * @param start inclusive, must be greater than 1
-   * @param end inclusive, must be less than Long.MAX_VALUE
-   * @return number of primes in the specified
-   */
-  int nPrimesInRange(final long start, final long end);
-
+    /** Returns the number of primes in the specified range.
+     *
+     * @param start inclusive, must be greater than 1
+     * @param end inclusive, must be less than Long.MAX_VALUE
+     * @return number of primes in the specified
+     */
+    int nPrimesInRange(final long start, final long end);
 }
